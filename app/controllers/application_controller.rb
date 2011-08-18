3d3ddce898f46c7ad5d70f0f845744d46cb60854
Yuri Golubchenko <yurig@yurig-pc.(none)>
2011-08-18 17:03:06 +0300

    finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-end
+  protect_from_forgery  asdqwdqwdq
+  qwdqdqdqw:a sdas dq dqw
+end                              asdasda
+sasdasd
